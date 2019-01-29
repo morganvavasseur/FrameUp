eb2a0c4dd7245a0cdaa52686ecb48d3a29e9e583
Morgan Vavasseur <morgan.vavasseur@gmail.com>
2019-01-29 11:38:19 +0100

    add layout event

--- app/src/main/java/com/example/frameup/HomeActivity.kt
+++ app/src/main/java/com/example/frameup/HomeActivity.kt
@@ -1,12 +1,12 @@
-package com.example.frameup
+package com.example.amaze
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class BasicActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_basic)
+        setContentView(R.layout.event_list)
     }
 }
