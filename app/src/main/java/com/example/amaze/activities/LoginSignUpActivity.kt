a2b1ca449d7568710041064a8cf021fd08ec235c
Yann <malanda70@gmail.com>
2019-04-25 09:20:55 +0200

    Autologin quand il y'a un token

--- app/src/main/java/com/example/amaze/activities/LoginSignUpActivity.kt
+++ app/src/main/java/com/example/amaze/activities/LoginSignUpActivity.kt
@@ -1,10 +1,14 @@
 package com.example.amaze.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.support.v4.view.ViewPager
+import com.example.amaze.AmazeApp
+import com.example.amaze.MainActivity
 import com.example.amaze.R
 import com.example.amaze.adapters.LoginSignUpPageAdapater
+import com.example.amaze.utils.SecureStorageServices
 
 class LoginSignUpActivity : FragmentActivity() {
 
@@ -14,12 +18,20 @@ class LoginSignUpActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_sign_up)
 
+        // Si le token à bien été stocké on connecte l'utilisateur à l'activité principale
+        if (SecureStorageServices.authJwtToken != null) {
+            val intent = Intent(AmazeApp.sharedInstance, MainActivity::class.java)
+            startActivity(intent)
+        }
+
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = findViewById(R.id.loginSignUpViewPager)
 
         // The pager adapter, which provides the pages to the view pager widget.
         val pagerAdapter = LoginSignUpPageAdapater(supportFragmentManager)
         mPager.adapter = pagerAdapter
+
+
 
     }
 }
