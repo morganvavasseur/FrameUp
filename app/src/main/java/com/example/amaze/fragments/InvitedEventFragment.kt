5511cb3bd2392108dec018553f9dcc67f4735b9c
Yann <malanda70@gmail.com>
2019-05-26 15:49:59 +0200

    Adjust Login fragment design

--- app/src/main/java/com/example/amaze/fragments/InvitedEventFragment.kt
+++ app/src/main/java/com/example/amaze/fragments/InvitedEventFragment.kt
@@ -54,7 +54,7 @@ class InvitedEventFragment : Fragment(), EventCardAdapter.OnEventCardListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var listener: HostedEventFragment.OnFragmentInteractionListener? = null
+    private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -110,5 +110,39 @@ class InvitedEventFragment : Fragment(), EventCardAdapter.OnEventCardListener {
         var intent = Intent(AmazeApp.sharedInstance, CreateEventActivity::class.java)
         startActivity(intent)
     }
-    
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment InvitedEventFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            InvitedEventFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
