25c56f6ea9ad6b0e4cdbd9a78c1d172d320c1260
Yann <malanda70@gmail.com>
2019-06-05 09:16:04 +0200

    Token login

--- app/src/main/java/com/example/amaze/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/amaze/fragments/LoginFragment.kt
@@ -81,9 +81,7 @@ class LoginFragment : Fragment() {
             override fun onResponse(call: Call<ConnectResults>, response: Response<ConnectResults>) {
                 var jwt = response.body()?.jwt
                 var user = response.body()?.user
-
-                Toast.makeText(AmazeApp.sharedInstance, "${jwt}", Toast.LENGTH_SHORT).show()
-
+                
                 // On vérifie que le token est bien
                 // une String puis on le stock de manière sécurisé
                 if (jwt is String){
